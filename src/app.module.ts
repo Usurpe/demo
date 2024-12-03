5186d530b319745100c67ea35fb49160f28b2b7c
Rayan <golamola54321@gmail.com>
2024-12-03 14:57:35 +0100

    added comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Property } from './Entities/property.entity';
-import { ApiExampleController } from './api-example/api-example.controller';
 
 
 @Module({
@@ -10,11 +9,10 @@ import { ApiExampleController } from './api-example/api-example.controller';
       url: "postgresql://demo_owner:rz4PclGjqo9R@ep-frosty-block-a5muddjd.us-east-2.aws.neon.tech/demo?sslmode=require",
       type: "postgres",
       port: 3306,
-      entities: [Property],
+      entities: [Property], // __dirname+'/**/*.entity{.ts,.js}' for all files that end with entity and .ts or .js
       synchronize: true
     }),
   ],
-  controllers: [ApiExampleController],
 })
 export class AppModule {}
 
